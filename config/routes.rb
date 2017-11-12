e7f28d153af58d60fca976dda5cf989ae5b190d2
justin skord <jskord@gmail.com>
2017-11-12 12:57:18 -0600

    added form to new page

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   get '/' => 'products#index'
   get '/products/new' => 'products#new'
-  
+  post '/products' => 'products#create'
   get '/products/:id' => 'products#show'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
