974dd0715c7af2e5862a3ecc993bee7b7d26fa9b
justin skord <jskord@gmail.com>
2017-11-13 21:31:02 -0600

    update action working in products database

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -26,6 +26,19 @@ class ProductsController < ApplicationController
   end
 
   def edit
+    product_id = params[:id]
+    @product = Product.find_by(id: product_id)
     render 'edit.html.erb'
+  end
+
+  def update
+    product_id = params[:id]
+    product = Product.find_by(id: product_id)
+    product.name = params[:name]
+    product.image = params[:image]
+    product.price = params[:price]
+    product.description = params[:description]
+    product.save
+    render 'update.html.erb'
   end
 end
