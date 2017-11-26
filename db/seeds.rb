741d2963ab501a458bedb45ae0f441a7e99c4cde
justin skord <jskord@gmail.com>
2017-11-26 12:29:29 -0600

    seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,10 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Product.create!([
+  {name: "Chinese Sword", price: "100.0", image: "http://www.kultofathena.com/images/swordsofchina.jpg", description: "a chinese sword!!!", instock: nil},
+  {name: "Scottish Sword", price: "1000.0", image: "https://www.darkknightarmoury.com/images/Category/medium/1442.png", description: "a sword from scotland!!!1", instock: nil},
+  {name: "African Sword", price: "50.0", image: "http://www.swordhistory.info/wp-content/uploads/nimcha-sword.jpg", description: "A sword from africa", instock: nil},
+  {name: "Swedish Sword", price: "400.0", image: "http://www.hoppersgiftware.co.uk/ekmps/shops/southernswords/images/swedish-viking-sword-scabbard-[3]-1097-p.jpg", description: "a sword from sweden!!", instock: nil},
+  {name: "Irish Sword", price: "50.0", image: "http://www.medieval-weaponry.co.uk/acatalog/ah3292-irish-ring-hilt-sword-scabbard-320.jpg", description: "a sword from ireland!!", instock: nil},
+  {name: "English Sword", price: "90.0", image: "http://www.darkknightarmoury.com/images/PRODUCT/large/SG281_1_.png", description: "The sword of King Arthur, of Camelot", instock: nil},
+  {name: "American Sword", price: "99.0", image: "https://iloveswords.com/images/specialty/SP_Sword_MDL101.jpg", description: "the American Liberty Sword", instock: nil},
+  {name: "Russian Sword", price: "25.0", image: "https://dqzrr9k4bjpzk.cloudfront.net/images/1583227/482613865.jpg", description: "a Russian Sword", instock: nil}
+])
